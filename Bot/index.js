cdaea5ef0a55e1c09735142b77f9330b75e75cf4
boversoneg <biznes.xeno@gmail.com>
2025-01-28 13:50:14 +0100

    Updated required properties for events

--- Bot/index.js
+++ Bot/index.js
@@ -65,10 +65,14 @@ const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'
 for (const file of eventFiles) {
 	const filePath = path.join(eventsPath, file);
 	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
+	if ('name' in event && 'execute' in event) {
+		if (event.once) {
+			client.once(event.name, (...args) => event.execute(...args));
+		} else {
+			client.on(event.name, (...args) => event.execute(...args));
+		}
 	} else {
-		client.on(event.name, (...args) => event.execute(...args));
+		console.log(`[WARNING] The event at ${filePath} is missing a required "name" or "execute" property.`);
 	}
 }
 
